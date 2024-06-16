5f9e23207b274894a0e29906befac9424f20dbbf
Ari Bahtiar <kewcoder@gmail.com>
2024-06-16 23:13:26 +0700

    test editable store design settings

--- app.vue
+++ app.vue
@@ -1,7 +1,31 @@
+<script setup>
+import { onMounted } from 'vue'
+
+const useTheme = () => useState('theme', () => {
+  return {
+    general: {},
+    sections: {}
+  }
+})
+const theme = useTheme() 
+
+onMounted(() => {
+  window.onmessage = function (e) {
+    if(e?.data?.event === 'store_design_changed'){
+      theme.value = e?.data?.data
+    }
+    console.log('new theme settings :',e?.data?.event)
+  };
+  window.top?.postMessage(
+      {
+          event: "request_store_design_settings",
+      },
+      "*",
+  );
+})
+</script>
 <template>
   <div>
-    is Development
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    {{  theme.general.top_banner_content }}
   </div>
 </template>
