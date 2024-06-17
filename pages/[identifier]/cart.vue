8666a793e764578d73fb497f4ca23887389dca19
Ari Bahtiar <kewcoder@gmail.com>
2024-06-17 11:58:39 +0700

    setup

---
+++ pages/[identifier]/cart.vue
@@ -0,0 +1,3 @@
+<template>
+    Cart Page
+</template>
---
+++ pages/[identifier]/index.vue
@@ -0,0 +1,7 @@
+<script>
+  // if custom domain check to pages
+  // if not custom domain check identifier and show home pages
+</script>
+<template>
+    Home Page
+</template>
